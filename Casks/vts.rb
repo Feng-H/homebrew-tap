ea11a6bc07271c775d79211104da08c7c5750e9f
GitHub Action <action@github.com>
2026-02-09 06:16:33 +0000

    Update VTS to 1.4.1

--- Casks/vts.rb
+++ Casks/vts.rb
@@ -1,8 +1,8 @@
 cask "vts" do
-  version "1.4.0"
-  sha256 "3631dd56604b66919a379d1df539610a4bbffa3a151d0716fc776eec62b05e88"
+  version "1.4.1"
+  sha256 "fe8d7ae228c9eadb4d425faed8086e4e90aa0bb0d6ef387a3310e6740db4c107"
 
-  url "https://github.com/Feng-H/VTS-silliconflow/releases/download/v1.4.0/VTS-1.4.0-Universal.dmg"
+  url "https://github.com/Feng-H/VTS-silliconflow/releases/download/v1.4.1/VTS-1.4.1-Universal.dmg"
   name "VTS"
   name "Voice Typing Studio"
   desc "Open-source macOS dictation replacement with AI-powered transcription"
