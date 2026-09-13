8d12c6a667beec6ad47273a5abcd6346008b9994
Feng-H <Feng-H@users.noreply.github.com>
2026-09-13 22:08:28 +0800

    SynDL 0.2.2

--- Casks/syndl.rb
+++ Casks/syndl.rb
@@ -1,8 +1,8 @@
 cask "syndl" do
-  version "0.2.1"
-  sha256 "728a6661f7222ac079aca3b5c2f664d393ab659eede002ae1d231a245f4f7ca0"
+  version "0.2.2"
+  sha256 "ea3971146e796e58514669cfa2c25ed6dbc2021a201e380b341c597e121ff1ef"
 
-  url "https://github.com/Feng-H/syn-download/releases/download/v0.2.1/SynDL-macos-applesilicon.zip"
+  url "https://github.com/Feng-H/syn-download/releases/download/v0.2.2/SynDL-macos-applesilicon.zip"
   name "SynDL"
   desc "Synology DSM multi-threaded resumable downloader (GUI)"
   homepage "https://github.com/Feng-H/syn-download"
